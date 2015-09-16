83604d774071a698da2b53bd397abf3d22aaef7c
Sagar Chinchavade <sagar_c@tekdi.net>
2015-09-16 15:09:25 +0530

    Changes for fixing save as copy  button not working

--- administrator/views/group/view.html.php
+++ administrator/views/group/view.html.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version     1.0.0
- * @package     com_tjfields
- * @copyright   Copyright (C) 2014. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      TechJoomla <extensions@techjoomla.com> - www.techjoomla.com
+ * @version    SVN: <svn_id>
+ * @package    Tjfields
+ * @author     Techjoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2015 TechJoomla. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
  */
 
 // No direct access
@@ -13,26 +13,37 @@ defined('_JEXEC') or die;
 jimport('joomla.application.component.view');
 
 /**
- * View to edit
+ * View class for editing country.
+ *
+ * @package     Tjfields
+ * @subpackage  com_tjfields
+ * @since       2.2
  */
 class TjfieldsViewGroup extends JViewLegacy
 {
 	protected $state;
+
 	protected $item;
+
 	protected $form;
 
 	/**
 	 * Display the view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
 	 */
 	public function display($tpl = null)
 	{
-		$this->state	= $this->get('State');
-		$this->item		= $this->get('Item');
-		$this->form		= $this->get('Form');
+		$this->state = $this->get('State');
+		$this->item  = $this->get('Item');
+		$this->form  = $this->get('Form');
 
 		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
-            throw new Exception(implode("\n", $errors));
+		if (count($errors = $this->get('Errors')))
+		{
+			throw new Exception(implode("\n", $errors));
 		}
 
 		$this->addToolbar();
@@ -41,32 +52,40 @@ class TjfieldsViewGroup extends JViewLegacy
 
 	/**
 	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
 	 */
 	protected function addToolbar()
 	{
 		JFactory::getApplication()->input->set('hidemainmenu', true);
 
-		$user		= JFactory::getUser();
-		$isNew		= ($this->item->id == 0);
-        if (isset($this->item->checked_out)) {
-		    $checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
-        } else {
-            $checkedOut = false;
-        }
-		$canDo		= TjfieldsHelper::getActions();
-		$input=JFactory::getApplication()->input;
-		$client=$input->get('client','','STRING');
+		$user  = JFactory::getUser();
+		$isNew = ($this->item->id == 0);
+
+		if (isset($this->item->checked_out))
+		{
+			$checkedOut = !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
+		}
+		else
+		{
+			$checkedOut = false;
+		}
+
+		$canDo           = TjfieldsHelper::getActions();
+		$input           = JFactory::getApplication()->input;
+		$client          = $input->get('client', '', 'STRING');
 		$component_title = JText::_('COM_TJFIELDS_TITLE_COMPONENT');
 
 		if (!empty($client))
 		{
 			$client = explode('.', $client);
 
-			if ($client['0']=='com_jticketing')
+			if ($client['0'] == 'com_jticketing')
 			{
 				$component_title = JText::_('COM_JTICKETING_COMPONENT');
 			}
-
 		}
 
 		if ($isNew)
@@ -88,25 +107,30 @@ class TjfieldsViewGroup extends JViewLegacy
 		}
 
 		// If not checked out, can save the item.
-		if (!$checkedOut && ($canDo->get('core.edit')||($canDo->get('core.create'))))
+		if (!$checkedOut && ($canDo->get('core.edit') || ($canDo->get('core.create'))))
 		{
-
 			JToolBarHelper::apply('group.apply', 'JTOOLBAR_APPLY');
 			JToolBarHelper::save('group.save', 'JTOOLBAR_SAVE');
 		}
-		if (!$checkedOut && ($canDo->get('core.create'))){
+
+		if (!$checkedOut && ($canDo->get('core.create')))
+		{
 			JToolBarHelper::custom('group.newsave', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
 		}
+
 		// If an existing item, can save to a copy.
-		if (!$isNew && $canDo->get('core.create')) {
+		if (!$isNew && $canDo->get('core.create'))
+		{
 			JToolBarHelper::custom('group.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
 		}
-		if (empty($this->item->id)) {
+
+		if (empty($this->item->id))
+		{
 			JToolBarHelper::cancel('group.cancel', 'JTOOLBAR_CANCEL');
 		}
-		else {
+		else
+		{
 			JToolBarHelper::cancel('group.cancel', 'JTOOLBAR_CLOSE');
 		}
-
 	}
 }

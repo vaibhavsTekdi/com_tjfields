0630bdc782d89eedd8f9201521abbbe06c3bd64e
ankush_m <ankush_m@techjoomla.com>
2016-12-26 17:01:08 +0530

    phpcs

--- administrator/tables/field.php
+++ administrator/tables/field.php
@@ -32,9 +32,9 @@ class TjfieldsTablefield extends JTable {
 	 * @see		JTable:bind
 	 * @since	1.5
 	 */
-	
+
 	public function bind($array, $ignore = '') {
-		
+
 		//print_r($array); die('asdasdasd');
 		$input = JFactory::getApplication()->input;
 		$task = $input->getString('task', '');
@@ -57,21 +57,21 @@ class TjfieldsTablefield extends JTable {
 			$actions = JFactory::getACL()->getActions('com_tjfields','field');
 			$default_actions = JFactory::getACL()->getAssetRules('com_tjfields.field.'.$array['id'])->getData();
 			$array_jaccess = array();
-		   
+
 			foreach($actions as $action){
 				$array_jaccess[$action->name] = $default_actions[$action->name];
 			}
 			$array['rules'] = $this->JAccessRulestoArray($array_jaccess);
-			
+
 		}
 		//Bind the rules for ACL where supported.
 		if (isset($array['rules']) && is_array($array['rules'])) {
 			$this->setRules($array['rules']);
 		}
 		return parent::bind($array, $ignore);
-		
+
 	}
-    
+
     /**
      * This function convert an array of JAccessRule objects into an rules array.
      * @param type $jaccessrules an arrao of JAccessRule objects.

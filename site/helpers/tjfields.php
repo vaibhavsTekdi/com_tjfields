7596f7bb9e529c95d151f03f94931c17091c8f28
“Komal <komal_m@tekditechnologies.com>
2018-03-19 12:05:50 +0530

    Task #123023 feat: Check field type is ucmsubform or subform

--- site/helpers/tjfields.php
+++ site/helpers/tjfields.php
@@ -231,7 +231,7 @@ class TjfieldsHelper
 
 					if (!empty($fvalue))
 					{
-						if ($field_data->type == 'subform' || $field_data->type == 'ucmsubform')
+						if ($field_data->type === 'subform' || $field_data->type === 'ucmsubform')
 						{
 							$this->saveSubformData($data, $fname, $field_data);
 						}

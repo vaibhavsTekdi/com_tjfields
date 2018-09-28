62c030599961f233358829a87b815cc672f4fb01
Amit Udale <amit_u@techjoomla.com>
2018-09-28 18:29:00 +0530

    Task #128279 chore : Subform with file upload

--- administrator/controllers/fields.json.php
+++ administrator/controllers/fields.json.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @version    SVN: <svn_id>
  * @package    Com_Tjfields
  * @author     Techjoomla <extensions@techjoomla.com>
  * @copyright  Copyright (c) 2009-2018 TechJoomla. All rights reserved.
